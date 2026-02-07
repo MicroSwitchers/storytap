33c3160b3bc427cbfbfb0af4226dffe435ea5147
MicroSwitchers <141078536+MicroSwitchers@users.noreply.github.com>
2026-02-07 17:54:50 -0500

    Add files via upload

---
+++ quickstories.js
@@ -0,0 +1,10 @@
+window.QUICK_STORIES = [
+    {
+        "title": "Snowy Day",
+        "text": "\"Maya!\" calls her mom. \"There is so much snow outside. Let's go out!\"\nMaya pulls on her snow pants with mom's help - pull, pull, pull.\nShe pushes her feet into her warm boots all by herself - stomp, stomp.\nMom pulls a soft hat over Maya's ears and Maya zips up her puffy coat.\n\"Mitts on!\" says Maya, wiggling her fingers inside her mitts.\nMom opens the door and whoosh - cold wind blows in!\nMaya steps outside and crunch, crunch, crunch go her boots.\nShe bends down and scoops up some cold white snow.\n\"It feels cold!\" Maya laughs.\nShe throws the snow high in the air and it drifts down all around her.\nLater, Maya's cheeks are pink and her nose is cold as they return inside to get warm."
+    },
+    {
+        "title": "Giraffe's New Friend",
+        "text": "Little Giraffe walked down to the pond with her Mom.\nShe bent her long, long neck all the way down for a drink.\nAnd there, in the water, was… another giraffe!\n\"Hello!\" said Little Giraffe.\nThe other giraffe didn't say hello back.\nBut it smiled when she smiled!\nIt blinked when she blinked!\nIt wiggled its ears when she wiggled her ears!\n\"Mama!\" called Little Giraffe. \"There's a giraffe in the water and she copies everything I do!\"\nMama laughed. \"That's you, little one. The water is just like a mirror!\"\nLittle Giraffe looked again. She stuck out her tongue — and so did the water giraffe!\nShe giggled and giggled.\n\"I like her,\" said Little Giraffe. \"She's silly — just like me!\"\nAfter a big drink, she trotted home, happy to know that her refelcion woule be there again the next day."
+    }
+];
